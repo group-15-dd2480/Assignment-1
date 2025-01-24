7e6ae27306e50c7d9826ba13023a37320ce88ad4
Markus Wessén <mwesse@kth.se>
2025-01-24 17:20:55 +0100

    feat: Add calculateDistanceBetween2Points (Issue #12)
    - Added function to calcualte distance between two points.
    - Added a test for the function

--- src/main/java/org/dd2480/Main.java
+++ src/main/java/org/dd2480/Main.java
@@ -30,6 +30,7 @@ public class Main {
         for (int i = 0; i < points.length - 1; i++)
             for (int j = i + 1; j < points.length; j++) {
                 // TODO: Change this distance calculation to the function proposed in Issue #4
+
                 // For example: if (dist(point[i], point[j]) > 2 * radius) return false;
                 double xDiff = points[j].getX() - points[i].getX();
                 double yDiff = points[j].getY() - points[i].getY();
@@ -38,6 +39,21 @@ public class Main {
                     return false;
             }
         return true;
+    }
+
+    /**
+     * 
+     * Function that corresponds to LIC 0
+     * 
+     * @param point1 a 2D point
+     * @param point2 a 2D point
+     * @return distance between point1 and point2
+     */
+    public static double calculateDistanceBetween2Points(Point2D point1, Point2D point2) {
+        double xDiff = point2.getX() - point1.getX();
+        double yDiff = point2.getY() - point1.getY();
+        double distance = Math.sqrt(xDiff * xDiff + yDiff * yDiff);
+        return distance;
     }
 
     /**
---
+++ src/test/java/org/dd2480/DistancefunctionTest.java
@@ -0,0 +1,18 @@
+package org.dd2480;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.awt.geom.Point2D;
+
+import org.junit.jupiter.api.Test;
+
+class DistancefunctionTest {
+
+    @Test
+    void distanceIsCorrect_GivenTwoPoints() {
+        Main.points = new Point2D[] { new Point2D.Double(1, 1), new Point2D.Double(2, 2)};
+
+        assertEquals(Math.sqrt(2), Main.calculateDistanceBetween2Points(Main.points[0], Main.points[1]));
+    }
+
+}
