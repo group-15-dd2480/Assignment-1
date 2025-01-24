821a102dd57f4ace6434b6e2dc06c1907041c31a
Markus Wessén <mwesse@kth.se>
2025-01-24 17:27:40 +0100

    refractor: replace distance calculation with function
    - Replace distance calculation in circleContainmentCheck with function calculateDistanceBetween2Points.

--- src/main/java/org/dd2480/Main.java
+++ src/main/java/org/dd2480/Main.java
@@ -29,12 +29,7 @@ public class Main {
             throw new IllegalArgumentException("Radius must be >= 0");
         for (int i = 0; i < points.length - 1; i++)
             for (int j = i + 1; j < points.length; j++) {
-                // TODO: Change this distance calculation to the function proposed in Issue #4
-
-                // For example: if (dist(point[i], point[j]) > 2 * radius) return false;
-                double xDiff = points[j].getX() - points[i].getX();
-                double yDiff = points[j].getY() - points[i].getY();
-                double distance = Math.sqrt(xDiff * xDiff + yDiff * yDiff);
+                double distance = calculateDistanceBetween2Points(points[j], points[i]);
                 if (distance > 2 * radius)
                     return false;
             }
