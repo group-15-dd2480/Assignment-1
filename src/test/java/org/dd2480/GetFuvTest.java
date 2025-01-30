be7a775d109188242403f97a7bf673f5c81bf308
aldenbro <david.aldenbro@gmail.com>
2025-01-30 11:33:45 +0100

    feat: Add more tests for FUV function
    - Added two more tests for `getFuv`.
    
    Closes #84

--- src/test/java/org/dd2480/GetFuvTest.java
+++ src/test/java/org/dd2480/GetFuvTest.java
@@ -76,9 +76,105 @@ class GetFuvTest {
         boolean[] funcFuv = Main.getFuv(pum, puv);
         boolean equalValues = true;
         for (int i = 0; i < fuv.length; i++)
-                if (funcFuv[i] != fuv[i])
-                    equalValues = false;
+            if (funcFuv[i] != fuv[i])
+                equalValues = false;
 
         assertEquals(true, equalValues, "incorrect values were produced in the PUM");
+    }
+
+    @Test
+    void shouldReturnFalseArray_whenGivenAllFalseParameters() {
+
+        // Smaller constructions are used to make it more human readable
+
+        boolean[][] subPum = {
+                { false, false, false, false },
+                { false, false, false, false },
+                { false, false, false, false },
+                { false, false, false, false },
+        };
+
+        boolean[] subPuv = { true, true, true, true }; // Has to be true for the PUM to be inspected
+
+        boolean[] subFuv = { false, false, false, false };
+
+        // Incorporating the constructions into the actual sized matrices/arrays
+
+        boolean[][] pum = new boolean[15][15];
+        for (int i = 0; i < pum.length; i++)
+            for (int j = 0; j < pum[0].length; j++)
+                pum[i][j] = true;
+        for (int i = 0; i < subPum.length; i++)
+            for (int j = 0; j < subPum[0].length; j++)
+                pum[i][j] = subPum[i][j];
+
+        boolean[] puv = new boolean[15];
+        for (int i = 0; i < puv.length; i++)
+            puv[i] = false;
+        for (int i = 0; i < subPuv.length; i++)
+            puv[i] = subPuv[i];
+
+        boolean[] fuv = new boolean[15];
+        for (int i = 0; i < fuv.length; i++)
+            fuv[i] = true;
+        for (int i = 0; i < subFuv.length; i++)
+            fuv[i] = subFuv[i];
+
+        // Checks if the calculated FUV is different from the true FUV
+        boolean[] funcFuv = Main.getFuv(pum, puv);
+        boolean equalValues = true;
+        for (int i = 0; i < fuv.length; i++)
+            if (funcFuv[i] != fuv[i])
+                equalValues = false;
+
+        assertEquals(true, equalValues, "a true value was incorrectly produced");
+    }
+
+    @Test
+    void shouldReturnTrueArray_whenGivenAllTrueParameters() {
+
+        // Smaller constructions are used to make it more human readable
+
+        boolean[][] subPum = {
+                { true, true, true, true },
+                { true, true, true, true },
+                { true, true, true, true },
+                { true, true, true, true },
+        };
+
+        boolean[] subPuv = { true, true, true, true };
+
+        boolean[] subFuv = { true, true, true, true };
+
+        // Incorporating the constructions into the actual sized matrices/arrays
+
+        boolean[][] pum = new boolean[15][15];
+        for (int i = 0; i < pum.length; i++)
+            for (int j = 0; j < pum[0].length; j++)
+                pum[i][j] = true;
+        for (int i = 0; i < subPum.length; i++)
+            for (int j = 0; j < subPum[0].length; j++)
+                pum[i][j] = subPum[i][j];
+
+        boolean[] puv = new boolean[15];
+        for (int i = 0; i < puv.length; i++)
+            puv[i] = false;
+        for (int i = 0; i < subPuv.length; i++)
+            puv[i] = subPuv[i];
+
+        boolean[] fuv = new boolean[15];
+        for (int i = 0; i < fuv.length; i++)
+            fuv[i] = true;
+        for (int i = 0; i < subFuv.length; i++)
+            fuv[i] = subFuv[i];
+
+        // Checks if the calculated FUV is different from the true FUV
+        boolean[] funcFuv = Main.getFuv(pum, puv);
+        boolean equalValues = true;
+        for (int i = 0; i < fuv.length; i++)
+            if (funcFuv[i] != fuv[i])
+                equalValues = false;
+
+        assertEquals(true, equalValues, "a true value was incorrectly produced");
     }
 }
